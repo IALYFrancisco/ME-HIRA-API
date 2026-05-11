1bf6ab6f28491305ff5bdd55fd015cf4673b798c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 11:19:38 +0300

    petite amélioration du service GetSong

--- app/services/song.js
+++ app/services/song.js
@@ -3,6 +3,7 @@ import { Song } from "../models/song.js";
 export async function GetSong(request, response){
     try{
         let songs = await Song.find()
+        songs = songs.filter((s)=> s.published === true)
         response.status(200).json(songs)
     }
     catch{
