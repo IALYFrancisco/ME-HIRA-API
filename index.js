498ccb56aa3d71e0fbc30b7807548d2e0b0884ea
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 11:32:48 +0300

    intégration globalement des configurations cors dans le projet

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import e from 'express'
 import { config } from 'dotenv'
 import { appRouter } from './app/routes/index.js'
 import { dbConnection } from './app/services/database.js'
+import { corsConfigurations } from './app/services/cors.js'
 
 config()
 const app = e()
 dbConnection()
 
+app.use(corsConfigurations)
 app.use(appRouter)
 
 app.listen(1234, ()=>{ console.log(`The application is running at ${process.env.APP_DOMAIN}`) })
