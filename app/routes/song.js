5aa531e51bad760e99635d42667aa962d4daad21
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-18 15:19:36 +0300

    création du route d'ajout de chanson dans la base de donéne

--- app/routes/song.js
+++ app/routes/song.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { GetSong } from "../services/song.js";
+import { AddSong, GetSong } from "../services/song.js";
 
 export const songRouter = Router()
 
 songRouter.get('/get', GetSong)
+songRouter.post('/add', AddSong)
