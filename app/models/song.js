e5b1ced1ba5f531608c1364a6da189023a665ade
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 19:35:41 +0300

    corréctions au niveau du modèle de donnée des chansons

--- app/models/song.js
+++ app/models/song.js
@@ -4,10 +4,10 @@ import slugify from "slugify"
 
 const songSchema = new Schema({
     title: { type: String, required: true },
-    author: { type: String, required: true },
+    author: { type: String },
     fileUrl: { type: String, required: true },
     album: { type: String },
-    composer: { type: String, required: true },
+    composer: { type: String },
     fileType: { type: String, required: true },
     singer: { type: Array, required: true },
     published: { type: Boolean, required: true, default: false },
