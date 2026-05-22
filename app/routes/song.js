c9c78f68748aacad33308d7eeaed36f2754c4cba
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-22 14:18:42 +0300

    définition du route de modification d'information de chanson

--- app/routes/song.js
+++ app/routes/song.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { AddSong, GetSong } from "../services/song.js";
+import { AddSong, GetSong, UpdateSong } from "../services/song.js";
 
 export const songRouter = Router()
 
 songRouter.get('/get', GetSong)
 songRouter.post('/add', AddSong)
-// songRouter.patch('/update')
+songRouter.patch('/update', UpdateSong)
