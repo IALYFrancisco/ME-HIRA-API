4f134b77e1b47cd7ff31d17824d0806fc9288be4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-22 17:52:51 +0300

    route de suppression de chanson définie

--- app/routes/song.js
+++ app/routes/song.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { AddSong, GetSong, UpdateSong } from "../services/song.js";
+import { AddSong, DeleteSong, GetSong, UpdateSong } from "../services/song.js";
 
 export const songRouter = Router()
 
 songRouter.get('/get', GetSong)
 songRouter.post('/add', AddSong)
 songRouter.patch('/update', UpdateSong)
+songRouter.delete('/remove', DeleteSong)
