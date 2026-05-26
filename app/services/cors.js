e9612c3fc45a78812efc3a0e91a8673258f7d98d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 16:40:26 +0300

    amélioration des configurations CORS pour les headers autorisés

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,5 +1,6 @@
 export function corsConfigurations(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST)
+    response.setHeader('Access-Control-Allow-Headers', "content-type")
     if(request.method === 'OPTIONS'){
         return response.sendStatus(200)
     }
