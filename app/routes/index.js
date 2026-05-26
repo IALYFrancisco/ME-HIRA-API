9b438da1586db98f997308d1a899ea4dd3da3f78
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 23:42:56 +0300

    ajout du routeur des actions d'authentification dans le projet

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { songRouter } from "./song.js";
+import { authenticationRouter } from "./authentication.js";
 
 export const appRouter = Router()
 
 appRouter.use('/song', songRouter)
+appRouter.use('/authentication', authenticationRouter)
