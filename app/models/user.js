14c9bbf3d0de40e8cc25acbc653cd0b97f039867
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 22:58:13 +0300

    modèle de donnée utilisateur défini

--- app/models/user.js
+++ app/models/user.js
@@ -1,2 +1,12 @@
 import { Schema, model } from "mongoose";
 
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    status: { type: String, required: true, default: 'user' },
+}, { timestamps: true })
+
+userSchema.set('optimisticConcurrency', true)
+
+export const User = new model('Users', userSchema)
