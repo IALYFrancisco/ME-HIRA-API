152c8e29e607462c7c4e1189bd98f04dabdf2aac
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 23:25:28 +0300

    toujours dans le développement du service de connexion à une compte uitlisateur me-hira

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -1,5 +1,20 @@
+import { compare } from "bcrypt"
+import { User } from "../models/user"
+import { sign } from "jsonwebtoken"
+
 export async function Login(request, response) {
- try{}
+ try{
+    const { email, password } = request.body
+    const user = await User.findOne({ email })
+    if(!user){
+        return response.status(401).end()
+    }
+    const match = await compare(password, user.password)
+    if(!match){
+        return response.status(401).end()
+    }
+    const accessToken = sign({ _id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" })
+ }
  catch{
     response.status(500).end()
  }
