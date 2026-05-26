b5999bca41a2a8f9c4177330d847235691c137bf
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-27 00:45:49 +0300

    définition de l'endpoint de déconnexion à un compte utilisateur me-hira

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -1,7 +1,8 @@
 import { Router } from "e"
-import { Login, RefreshToken } from "../services/authentication"
+import { Login, Logout, RefreshToken } from "../services/authentication"
 
 export const authenticationRouter = Router()
 
 authenticationRouter.post('/login', Login)
 authenticationRouter.post('/refresh-token', RefreshToken)
+authenticationRouter.post('/refresh-token', Logout)
