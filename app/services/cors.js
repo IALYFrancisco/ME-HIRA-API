f6faf2518878c114ecb99770d225141aeac9d3aa
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 16:38:53 +0300

    mis à jour des configurations CORS pour la réponse raide des verb OPTIONS

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,4 +1,7 @@
 export function corsConfigurations(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST)
+    if(request.method === 'OPTIONS'){
+        return response.sendStatus(200)
+    }
     next()
 }
