e33104815a6b286fe1e31dceb74f12ac05c5609b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-27 00:44:49 +0300

    définition de l'endpoint de rafraîchissement de token

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -1,6 +1,7 @@
 import { Router } from "e"
-import { Login } from "../services/authentication"
+import { Login, RefreshToken } from "../services/authentication"
 
 export const authenticationRouter = Router()
 
 authenticationRouter.post('/login', Login)
+authenticationRouter.post('/refresh-token', RefreshToken)
