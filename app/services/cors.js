8923e39c47a96662ebaa967515e448977ae1c1d1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 17:40:50 +0300

    abandont des configurations CORS ajoutées précédement

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,9 +1,4 @@
 export function corsConfigurations(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST)
-    response.setHeader('Access-Control-Allow-Headers', "content-type")
-    response.setHeader('Access-Control-Allow-Methods', 'GET,POST')
-    if(request.method === 'OPTIONS'){
-        return response.sendStatus(200)
-    }
     next()
 }
