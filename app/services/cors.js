2d1fe21dbe7b42120a075bc0f7cddf213baf3da3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-26 17:53:30 +0300

    ajout d'une nouvelle configurations cors (autorisation des en-têtes spécifiques)

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,4 +1,5 @@
 export function corsConfigurations(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST)
+    response.setHeader('Access-Control-Allow-Headers', process.env.ALLOWED_HEADERS)
     next()
 }
