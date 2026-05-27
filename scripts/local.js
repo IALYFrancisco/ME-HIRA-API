2cf80b70d496831d33a7db52b43d74f538bdb567
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-27 20:29:42 +0300

    suppression d'importation de chalk

---
+++ scripts/local.js
@@ -0,0 +1,43 @@
+import { CheckSuperuserAndHisEmail, CreateSuperuser, superuserPassword } from "./services/index.js";
+import fs from 'fs'
+import path from "path";
+import os from 'os'
+import { config } from "dotenv";
+import { DbConnection } from '../app/services/db_connection.js'
+import { disconnect } from "mongoose";
+
+config({ quiet: true })
+
+async function CreateSuperuserAndSaveLocal(){
+    try{
+        DbConnection()
+        let checkingResult = await CheckSuperuserAndHisEmail()
+        if(checkingResult === true){
+            return
+        }else if(checkingResult === false){
+            let homedir = os.homedir()
+            let superuserInfosLocation = path.join(
+                homedir,
+                '.luminischool',
+                'superuser'
+            )
+            fs.mkdirSync(superuserInfosLocation, { recursive: true })
+            let fileContents = `{name: '${process.env.SUPERUSER_NAME}',email: '${process.env.SUPERUSER_EMAIL}',status: 'superuser',password: '${superuserPassword}'}`
+            let filePath = path.join(superuserInfosLocation, 'informations.json')
+            fs.writeFileSync(filePath, fileContents, 'utf-8')
+            console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")(`Superuser informations are saved at ${filePath}`))
+            await CreateSuperuser()
+            console.log(chalk.bgHex('#098702ff').hex('#fffbfc')('Done!'))
+        }else if(checkingResult === undefined){
+            return
+        }
+    }
+    catch(err){
+        console.log(err)
+        console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error saving local the superuser informations.'))
+    }finally{
+        await disconnect()
+    }
+}
+
+CreateSuperuserAndSaveLocal()
