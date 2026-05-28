dbfa05720b11370859f2c54c7ddd2ac330b39be6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-28 09:07:44 +0300

    le middleware isAdminOrSuperuser est défini

---
+++ app/services/user.js
@@ -0,0 +1,8 @@
+export function isAdminOrSuperuser(request, response, next) {
+    let user = request
+    if(user.status === "admin" || user.status === "superuser"){
+        next()
+    }else{
+        response.status(409).end()
+    }
+}
