d3e19d0f0105f16aceb46c65820fc18014fa4830
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-30 11:15:41 +0300

    ajout d'une configuration cors

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,5 +1,6 @@
 export function corsConfigurations(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST)
     response.setHeader('Access-Control-Allow-Headers', process.env.ALLOWED_HEADERS)
+    response.setHeader('Access-Control-Allow-Credentials', true)
     next()
 }
