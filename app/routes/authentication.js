36c4be612094f24fd4637c3b76acc8c3cd0ea014
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 06:58:11 +0300

    ajout du middleware isNotAuthenticated sur l'endpoint de connexion à un compte utilisateur me-hira

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
-import { Login, Logout, RefreshToken } from "../services/authentication.js"
+import { isNotAuthenticated, Login, Logout, RefreshToken } from "../services/authentication.js"
 
 export const authenticationRouter = Router()
 
-authenticationRouter.post('/login', Login)
+authenticationRouter.post('/login', isNotAuthenticated, Login)
 authenticationRouter.post('/refresh-token', RefreshToken)
 authenticationRouter.post('/logout', Logout)
