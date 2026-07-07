94aee7a5e311eda6d325e2ac81e7c9a9aa20ac5f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-07 10:17:42 +0300

    ajout d'un système de concurrence de changement de document artist via mongoose

--- app/models/artist.js
+++ app/models/artist.js
@@ -9,3 +9,5 @@ const artistSchema = new Schema({
     image: { type: String, required: true }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
     birthDayAndPlace : { type: String, required: true }
 })
+
+artistSchema.set("optimisticConcurrency", true)
