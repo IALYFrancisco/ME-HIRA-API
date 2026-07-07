a53f719aafa8d3d9d8e73373de08b87a0fff7239
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-07 10:26:36 +0300

    modèle des données contacts d'artiste défini

--- app/models/artistContact.js
+++ app/models/artistContact.js
@@ -1,7 +1,11 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const contactArtistSchema = new Schema({
     artistId: { type: Schema.Types.ObjectId, ref: "Artist", required: true },
     phoneNumber: { type: String },
     email: { type: String }
 })
+
+contactArtistSchema.set("optimisticConcurrency", true)
+
+export const ContactArtist = new model('ContactArtist', contactArtistSchema)
