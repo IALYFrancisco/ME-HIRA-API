84c9226401b5e826b984537516f5d477d27815cb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-07 10:14:46 +0300

    développement encore du modèle de donnée pour les documents Artist

--- app/models/artist.js
+++ app/models/artist.js
@@ -4,4 +4,7 @@ const artistSchema = new Schema({
     name: { type: String, required: true }, /** this is the full real name of the subject */
     artistName: { type: String, required: true },
     roles: { type: Array, required: true },
+    about: { type: String, required: true },
+    address: { type: String, required: true },
+    image: { type: String, required: true }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
 })
