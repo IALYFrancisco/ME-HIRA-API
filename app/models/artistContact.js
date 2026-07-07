5f5e3a82bcb5e17482b7bdb5c928ea37d1216c33
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-07 10:24:27 +0300

    toujours dans le développement du modèle des données contacts d'un artiste

--- app/models/artistContact.js
+++ app/models/artistContact.js
@@ -1,5 +1,7 @@
 import { Schema } from "mongoose";
 
 const contactArtistSchema = new Schema({
-    artistId: { type: Schema.Types.ObjectId, ref: "Artist", required: true }
+    artistId: { type: Schema.Types.ObjectId, ref: "Artist", required: true },
+    phoneNumber: { type: String },
+    email: { type: String }
 })
