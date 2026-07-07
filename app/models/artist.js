2a9874de378bdd4932ef4515d6f11942586382c5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-07 10:11:07 +0300

    développement encore du modèle des données pour les documents Artist

--- app/models/artist.js
+++ app/models/artist.js
@@ -1,1 +1,7 @@
 import { Schema } from "mongoose";
+
+const artistSchema = new Schema({
+    name: { type: String, required: true }, /** this is the full real name of the subject */
+    artistName: { type: String, required: true },
+    roles: { type: Array, required: true },
+})
