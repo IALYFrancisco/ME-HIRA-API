aa9b3e8821351ada1fe5c0deddbc331ad969fc0e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-08 14:01:16 +0300

    toujours dans la configuration des routes des actions sur les documents artist

--- app/routes/artist.js
+++ app/routes/artist.js
@@ -1,2 +1,4 @@
 import { Router } from "express"
 import { GetArtist } from "../services/artist.js"
+import { isAuthenticated } from "../services/authentication.js";
+import { isAdminOrSuperuser } from "../services/user.js";
