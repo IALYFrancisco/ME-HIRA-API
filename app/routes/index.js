afda4bc6d21176a6b468d530bc6b33265deaee9c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-08 14:06:27 +0300

    corréction de nom de variable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { songRouter } from "./song.js";
 import { authenticationRouter } from "./authentication.js";
-import { userRouer } from "./user.js";
+import { userRouter } from "./user.js";
 import { artistRouter } from "./artist.js";
 
 export const appRouter = Router()
 
 appRouter.use('/song', songRouter)
 appRouter.use('/authentication', authenticationRouter)
-appRouter.use('/user', userRouer)
+appRouter.use('/user', userRouter)
 appRouter.use('/artist', artistRouter)
--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { isAuthenticated } from "../services/authentication.js";
 import { GetCurrentUserInformations } from "../services/user.js";
 
-export const userRouer = Router()
+export const userRouter = Router()
 
-userRouer.get('/informations', isAuthenticated, GetCurrentUserInformations)
+userRouter.get('/informations', isAuthenticated, GetCurrentUserInformations)
