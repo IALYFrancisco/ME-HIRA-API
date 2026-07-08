5bff670ea56358a302ecf51e0b223792cd9b830f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-08 13:56:22 +0300

    toujours dans le développement du service GetArtist

--- app/services/artist.js
+++ app/services/artist.js
@@ -1,5 +1,10 @@
+import { Artist } from "../models/artist.js";
+
 export async function GetArtist(request, response){
-    try{}
+    try{
+        let artists = await Artist.find()
+        response.status(200).json(artists)
+    }
     catch{
         response.status(500).end()
     }
