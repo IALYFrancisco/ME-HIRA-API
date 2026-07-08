a9f05d26ab44a1de75cf537c421d5e1f793df915
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-08 14:02:11 +0300

    configuration encore des routes des services sur les documents artist

--- app/routes/artist.js
+++ app/routes/artist.js
@@ -2,3 +2,5 @@ import { Router } from "express"
 import { GetArtist } from "../services/artist.js"
 import { isAuthenticated } from "../services/authentication.js";
 import { isAdminOrSuperuser } from "../services/user.js";
+
+export const artistRouter = Router()
