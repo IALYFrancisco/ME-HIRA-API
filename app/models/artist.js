c31e4dfb7bedfb86c0f615ae9e285b8ce8ee88bb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-11 11:13:23 +0300

    l'attribut 'image' est désormais facultatif dans un document 'Artist'

--- app/models/artist.js
+++ app/models/artist.js
@@ -6,7 +6,7 @@ const artistSchema = new Schema({
     roles: { type: Array, required: true },
     about: { type: String },
     address: { type: String },
-    image: { type: String, required: true }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
+    image: { type: String }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
     birthDayAndPlace : { type: String, required: true }
 })
 
