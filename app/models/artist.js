a3b50f60cc6b505d053db11ebbb2ec07d16e7a65
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-11 11:11:10 +0300

    l'attribut 'about' est désormais facultatif dans un document 'Artist'

--- app/models/artist.js
+++ app/models/artist.js
@@ -4,7 +4,7 @@ const artistSchema = new Schema({
     name: { type: String }, /** this is the full real name of the subject */
     artistName: { type: String, required: true },
     roles: { type: Array, required: true },
-    about: { type: String, required: true },
+    about: { type: String },
     address: { type: String, required: true },
     image: { type: String, required: true }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
     birthDayAndPlace : { type: String, required: true }
