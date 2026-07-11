2ae50366860e8bc942dd4029c6d67e6e4d8e458e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-11 11:10:01 +0300

    l'attribut 'name' est actuelement facultatif dans un document 'Artist'

--- app/models/artist.js
+++ app/models/artist.js
@@ -1,7 +1,7 @@
 import { model, Schema } from "mongoose";
 
 const artistSchema = new Schema({
-    name: { type: String, required: true }, /** this is the full real name of the subject */
+    name: { type: String }, /** this is the full real name of the subject */
     artistName: { type: String, required: true },
     roles: { type: Array, required: true },
     about: { type: String, required: true },
