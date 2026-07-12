afc34efd45867a0724860816624ee0844e31bead
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-12 10:43:18 +0300

    elèvement des attributs requied sur les champs 'normalizedArtistName' et 'artistName'

--- app/models/artist.js
+++ app/models/artist.js
@@ -3,14 +3,14 @@ import { normalizeText } from "../services/song.js";
 
 const artistSchema = new Schema({
     name: { type: String }, /** this is the full real name of the subject */
-    artistName: { type: String, required: true },
+    artistName: { type: String },
     roles: { type: Array, required: true },
     about: { type: String },
     address: { type: String },
     image: { type: String }, /** this attribut contains the url to the image of sabject, it can be an image of only subject or an image of group whit precisions */
     birthDayAndPlace : { type: String },
     normalizedName: { type: String },
-    normalizedArtistName: { type: String, required: true }
+    normalizedArtistName: { type: String }
 })
 
 artistSchema.set("optimisticConcurrency", true)
