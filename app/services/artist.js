78d9c61cf71cf7f0ea039d38c357de3f9b552ee5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-12 12:59:16 +0300

    développement du système dans GetArtist si un prompt est envoyé par l'utilisateur

--- app/services/artist.js
+++ app/services/artist.js
@@ -4,7 +4,11 @@ import { ContactArtist } from "../models/artistContact.js";
 export async function GetArtist(request, response){
     try{
 
-        if(request.query?.prompt && request.query.prompt.trim() !== ""){
+        const { prompt } = request.query
+
+        if(prompt && prompt.trim() !== ""){
+
+            
             
         }
 
