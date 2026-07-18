d6db021726f6b3ec754b4199fb2a0d6db2f84d6f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 12:30:23 +0300

    endpoint de modification des informations utilisateurs définis

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { isAuthenticated } from "../services/authentication.js";
-import { CheckUser, GetCurrentUserInformations, isAdminOrSuperuser } from "../services/user.js";
+import { CheckUser, GetCurrentUserInformations, isAdminOrSuperuser, UpdateUser } from "../services/user.js";
 
 export const userRouter = Router()
 
 userRouter.get('/informations', isAuthenticated, GetCurrentUserInformations)
 userRouter.post('/check', isAuthenticated, isAdminOrSuperuser, CheckUser)
+userRouter.patch('/update', isAuthenticated, isAdminOrSuperuser, UpdateUser)
