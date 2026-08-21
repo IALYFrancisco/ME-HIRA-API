f7b4e104f9fbbe521e9149fcf9188a9d2cffac78
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-21 10:20:19 +0300

    développement encore du service de vérification d'email

--- app/services/user.js
+++ app/services/user.js
@@ -64,6 +64,10 @@ export async function UpdateUser(request, response){
     }
 }
 
+// Service permettant de vérifier si l'email fourni est associé à un count me-hira avant tout action de mot de passe oublié. 
 export async function ForgottenPasswordCheckAccount(request, response) {
-    
+    try{}
+    catch{
+        return response.status(500).end()
+    }
 }
