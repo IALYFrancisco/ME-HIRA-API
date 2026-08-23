33621d292e63d10b18c33eb143282364f28dc6df
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-23 19:02:54 +0300

    changement de service associé au route 'forgotten-password'

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { isAuthenticated, isNotAuthenticated } from "../services/authentication.js";
-import { CheckUser, GetCurrentUserInformations, isAdminOrSuperuser, UpdateUser } from "../services/user.js";
+import { CheckUser, GetCurrentUserInformations, isAdminOrSuperuser, UpdateUser, ForgottenPasswordCheckAccount } from "../services/user.js";
 
 export const userRouter = Router()
 
 userRouter.get('/informations', isAuthenticated, GetCurrentUserInformations)
 userRouter.post('/check', isAuthenticated, isAdminOrSuperuser, CheckUser)
 userRouter.patch('/update', isAuthenticated, isAdminOrSuperuser, UpdateUser)
-userRouter.post('/forgotten-password', isNotAuthenticated, UpdateUser)
+userRouter.post('/forgotten-password', isNotAuthenticated, ForgottenPasswordCheckAccount)
