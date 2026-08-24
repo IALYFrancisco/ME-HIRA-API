c634ed747dbf2dec9d5574c8a4673c5794e97a67
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-24 14:27:08 +0300

    endpoint de vérification de token de réinitialisation de mot de passe défini

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { isAuthenticated, isNotAuthenticated } from "../services/authentication.js";
+import { CheckResetPasswordToken, isAuthenticated, isNotAuthenticated } from "../services/authentication.js";
 import { CheckUser, GetCurrentUserInformations, isAdminOrSuperuser, UpdateUser, ForgottenPasswordCheckAccount } from "../services/user.js";
 
 export const userRouter = Router()
@@ -8,3 +8,4 @@ userRouter.get('/informations', isAuthenticated, GetCurrentUserInformations)
 userRouter.post('/check', isAuthenticated, isAdminOrSuperuser, CheckUser)
 userRouter.patch('/update', isAuthenticated, isAdminOrSuperuser, UpdateUser)
 userRouter.post('/forgotten-password', isNotAuthenticated, ForgottenPasswordCheckAccount)
+userRouter.post('/check-k', isNotAuthenticated, CheckResetPasswordToken)
