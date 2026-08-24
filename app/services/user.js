b8afede2399a4fc8a3fdea2135563ffa264c0636
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-24 08:25:50 +0300

    intégration de template email pour l'email de réinitialisation de mot de passe

--- app/services/user.js
+++ app/services/user.js
@@ -72,6 +72,39 @@ export async function ForgottenPasswordCheckAccount(request, response) {
         const user = await User.findOne({ email: email })
         if(user){
             const resetPasswordToken = randomBytes(32).toString("hex")
+
+            const templateEmail = `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+                    <style>
+                        .lato-regular {
+                            font-family: "Lato", sans-serif;
+                            font-weight: 400;
+                            font-style: normal;
+                        }
+                    </style>
+                    <title>Me-Hira - Réinitialisation de mot de passe</title>
+                </head>
+                <body>
+                    <section style="max-width: 300px; margin: 100px auto; text-align: center; color: #191919;">
+                        <a href="https://mehira.onrender.com" target="_blank" style="width: max-content; margin: auto;">
+                            <img src="https://mehira.onrender.com/images/logo-de-me-hira.png" alt="logo du plateforme me-hira" style="width: 65px;">
+                        </a>
+                        <p class="lato-regular">Bonjour <strong>Jean Joseph Rabearivelo</strong>, veuillez clicker sur le bouton ci-dessous afin de réinitialiser le mot de passe de votre compte utilisateur <span style="background-color: #FEC700; padding: 3px 5px;border-radius: 5px;font-size: 13px;">Me-Hira</span> .</p>
+                        <a target="_blank" href="https://mehira.onrender.com/authentication/reset-password" style="display: block;border: 1px solid #FEC700;padding: 2px;width: fit-content;height: fit-content;border-radius: 12px; margin: 40px auto;">
+                            <button class="lato-regular" style="padding: 10px 15px;background-color: #FEC700;border: none;border-radius: 10px;cursor: pointer; color: #191919;">Réinitialiser mon mot de passe</button>
+                        </a>
+                    </section>
+                </body>
+                </html>
+            `
+
         }
         return response.status(200).end()
     }
