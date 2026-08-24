1f581a5031bbdedd98cbc932f652161af3a546d5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-24 18:24:14 +0300

    petite amélioration du modèle de donnée 'ResetPasswordToken'

--- app/models/resetPasswordToken.js
+++ app/models/resetPasswordToken.js
@@ -3,7 +3,7 @@ import { model, Schema } from "mongoose";
 const resetPasswordTokenSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     hashedToken : { type: String, required: true },
-    expiresAt : { type: Date, require: true },
+    expiresAt : { type: Date, required: true },
     used: { type: Boolean, default: false, required: true }
 }, { timestamps: true })
 
