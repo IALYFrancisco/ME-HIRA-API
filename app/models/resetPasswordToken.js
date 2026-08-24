0e1f130561b5daa4166f56a73084e643c4d49eaa
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-24 08:59:08 +0300

    définition du modèle de donnée tokens de réinitialisation de mot de passe

--- app/models/resetPasswordToken.js
+++ app/models/resetPasswordToken.js
@@ -1,5 +1,8 @@
 import { model, Schema } from "mongoose";
 
 const resetPasswordTokenSchema = new Schema({
-    
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    hashedToken : { type: String, required: true },
+    expiresAt : { type: Date, require: true },
+    used: { type: Boolean, default: false, required: true }
 })
