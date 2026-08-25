8897474193f7c5a765f2965e97b80afb1e5a0568
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-25 06:00:35 +0300

    ajout d'un index ttl sur le modèle de donnée 'ResetPasswordToken'

--- app/models/resetPasswordToken.js
+++ app/models/resetPasswordToken.js
@@ -3,7 +3,13 @@ import { model, Schema } from "mongoose";
 const resetPasswordTokenSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     hashedToken : { type: String, required: true },
-    expiresAt : { type: Date, required: true },
+    expiresAt : { 
+        type: Date,
+        required: true,
+        index: {
+            expireAfterSeconds: 0
+        }
+    },
     used: { type: Boolean, default: false, required: true }
 }, { timestamps: true })
 
