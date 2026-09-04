5e127b3d098a091b0c8c420be1a115823bd2753b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-04 12:48:55 +0300

    ajout de middleware d'upload de fichier image profile d'artite sur la route de création de document artiste

--- app/routes/artist.js
+++ app/routes/artist.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
-import { CreateArtistDocument, DeleteArtistDocument, GetArtist, UpdateArtistDocument } from "../services/artist.js"
+import { CreateArtistDocument, DeleteArtistDocument, GetArtist, UpdateArtistDocument, upload } from "../services/artist.js"
 
 export const artistRouter = Router()
 
 artistRouter.get('/get', GetArtist)
-artistRouter.post('/create-document', CreateArtistDocument)
+artistRouter.post('/create-document', upload.single("artistProfile"), CreateArtistDocument)
 artistRouter.patch('/update', UpdateArtistDocument)
 artistRouter.delete('/delete', DeleteArtistDocument)
